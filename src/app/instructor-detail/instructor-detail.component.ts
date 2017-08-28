c5a00e7748578390ee53688ef9926c868a70e973
Alena Golovina <alenasgolovina@gmail.com>
2017-08-27 23:58:16 -0700

    add dynamic routing functionality to retrieve parameters with -the fat arrow notation-

--- src/app/instructor-detail/instructor-detail.component.ts
+++ src/app/instructor-detail/instructor-detail.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Instructor } from '../instructor.model';
 @Component({
   selector: 'app-instructor-detail',
   templateUrl: './instructor-detail.component.html',
   styleUrls: ['./instructor-detail.component.css']
 })
 export class InstructorDetailComponent implements OnInit {
-
-  constructor() { }
+instructorId: number = null;
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.instructorId = parseInt(urlParameters['id']);
+    });
   }
 
 }
